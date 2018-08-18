2f2b66384b6816824be44be79d51892d92e8f73f
Shined Angel <36896864+barbiierror@users.noreply.github.com>
2018-08-18 15:49:15 -0300

    Commit at 18/08

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Qual o seu nome?");
+            var name = Console.ReadLine();
+            var date = DateTime.Now;
+            Console.WriteLine($"/nOlá, {name}, esse programa está rodando em {date:d} as {date:t}");
+            Console.Write("Pressione qualquer tecla para sair...");
+            Console.ReadKey(true);
+        }
+    }
+}
